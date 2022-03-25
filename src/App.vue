4f2c7033bebf36a1125ac1a9a22d9dca09558d7e
“zhou62396272” <454367781@qq.com>
2022-03-25 12:07:28 +0800

    zxc

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="width">
+    <a-form :model="bucketInfo">
+      <a-form-item
+        label="Bucket名称"
+        name="bucketName"
+        :rules="rule.bucketNameRules()"
+      >
+        <a-input style="width: 500px;" v-model:value="bucketInfo.bucketName" :maxlength="63" placeholder="Bucket名称" />
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import "ant-design-vue/dist/antd.css";
+import { reactive } from "vue";
+import axios from "axios";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const bucketInfo = reactive({
+  bucketName: ''
+})
+
+const abortPromise = (promise1) => {
+  let abort;
+  const promise2 = new Promise((resolve, reject) => {
+    abort = reject;
+  });
+  const p = Promise.race([promise1, promise2]);
+  p.abort = abort;
+  return p;
+};
+
+const debouncePromise = (success, fail, time) => {
+  let promise;
+  return function (...rest) {
+    if (promise && typeof promise.abort === "function") {
+      promise.abort();
+    }
+    const timeoutPromise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(undefined);
+      }, time);
+    });
+    promise = abortPromise(timeoutPromise);
+    return promise.then(
+      () => {
+        return success(...rest);
+      },
+      () => {
+        return fail(...rest);
+      }
+    );
+  };
+};
+
+const fail = () => {
+  console.log("由于防抖中断了第一次的请求");
+  return Promise.resolve(); // 忽视它暂时是正确的
+};
+
+const success = (rule, value) => {
+  console.log("请求后台接口");
+  const reg = /^(?!(-|[-a-z0-9]*--|[-a-z0-9]*-$))[-a-z0-9]*[-a-z0-9]*$/;
+  if (value.length < 1) {
+    return Promise.reject("名称不能为空");
   }
-}
+  if (value.length < 3 || value.length > 63) {
+    return Promise.reject("请输入 3~63 个字符");
+  } else if (!reg.test(value)) {
+    return Promise.reject("只允许小写字母、数字、短横线（-），且不能以短横线开头或结尾");
+  }
+  // 调用接口查询是否重复
+  axios
+    .get(
+      "https://www.fastmock.site/mock/a535d266440f87553f2748030e9505cb/starship-ss3/api/check",
+      value
+    )
+    .then((res) => {
+      console.log(res.data);
+    });
+};
+
+const check = debouncePromise(success, fail, 200);
+
+const rule = {
+  bucketNameRules() {
+    return { validator: check, trigger: "change", required: true };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +98,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.width {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.w500 {
+  width: 500px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Antd)
+app.mount('#app')
